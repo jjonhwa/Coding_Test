43f87f081bd400b93e864ebea0b174d8f011a465
jjonhwa <53552847+jjonhwa@users.noreply.github.com>
2022-04-05 11:19:01 +0900

    Update 짝지어제거하기.py

--- 프로그래머스/solve/짝지어제거하기.py
+++ 프로그래머스/solve/짝지어제거하기.py
@@ -12,3 +12,25 @@ def solution(s) :
         return 1
     else :
         return 0
+
+    
+from collections import deque
+
+def solution(s):    
+    string = deque(s)
+    stack = []
+    while string:
+        spell = string.popleft()
+        stack.append(spell)
+        
+        while len(stack) >= 2:
+            if stack[-1] == stack[-2]:
+                stack.pop()
+                stack.pop()
+            else:
+                break
+    
+    if stack:
+        return 0
+    else:
+        return  1
